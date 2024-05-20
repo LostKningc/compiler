a717b213b0a22510ff611c831bcf6ebaf5bd1474
lirui <2158644431@qq.com>
2024-05-20 15:20:16 +0800

    add function to deal with sub, eq in IR

--- IRtoAsm/test.s
+++ IRtoAsm/test.s
@@ -1,4 +1,8 @@
 .text
 .globl main
 main:
+li    t0, 0
+sub   t1, 0, t0
+sub   t2, 0, t1
+mv    a0, t2
 ret
--- IRtoAsm/visit.cpp
+++ IRtoAsm/visit.cpp
@@ -1,10 +1,17 @@
 #include "../include/IRtoAsm.hpp"
-
+std::map<koopa_raw_binary_t *, std::string> reg_map;
+std::stack<std::string> reg_stack;
 // 访问 raw program
 void Visit(const koopa_raw_program_t &program)
 {
     // 执行一些其他的必要操作
     // ...
+    // 初始化寄存器栈
+    for (int i = 6; i >= 0; --i)
+    {
+        std::string str = "t" + std::to_string(i);
+        reg_stack.push(str);
+    }
 
     std::cout << ".text" << std::endl;
     std::cout << ".globl main" << std::endl;
@@ -47,7 +54,7 @@ void Visit(const koopa_raw_slice_t &slice)
 void Visit(const koopa_raw_function_t &func)
 {
 
-    std::cout << func->name+1 << ":" << std::endl;
+    std::cout << func->name + 1 << ":" << std::endl;
 
     // 执行一些其他的必要操作
     // ...
@@ -69,6 +76,8 @@ void Visit(const koopa_raw_value_t &value)
 {
     // 根据指令类型判断后续需要如何访问
     const auto &kind = value->kind;
+    // 设置左对齐
+    std::cout << std::left;
     switch (kind.tag)
     {
     case KOOPA_RVT_RETURN:
@@ -93,19 +102,117 @@ void Visit(const koopa_raw_value_t &value)
 // 访问返回指令
 void Visit(const koopa_raw_return_t &ret)
 {
-    Visit(ret.value);
+    // Visit(ret.value);
+    switch (ret.value->kind.tag)
+    {
+    case KOOPA_RVT_BINARY:
+        std::cout << std::setw(6) << "mv" << "a0, " << reg_map[(koopa_raw_binary_t *)&ret.value->kind.data.binary] << std::endl;
+        break;
+    case KOOPA_RVT_INTEGER:
+        // Visit(ret.value);
+        std::cout << std::setw(6) << "li" << "a0, " << ret.value->kind.data.integer.value << std::endl;
+        break;
+    };
     std::cout << "ret" << std::endl;
 }
 
 // 访问整数指令
-void Visit(const koopa_raw_integer_t &integer)
+std::string Visit(const koopa_raw_integer_t &integer)
 {
-    std::cout << "li a0, " << integer.value << std::endl;
+    std::string reg;
+    if (reg_stack.empty())
+        assert(false);
+    reg = reg_stack.top();
+    reg_stack.pop();
+    std::cout << std::setw(6) << "li" << reg << ", " << integer.value << std::endl;
+    return reg;
 }
 
 void Visit(const koopa_raw_binary_t &binary)
 {
-    
+    std::string reg;
+    if (reg_stack.empty())
+        assert(false);
+    reg = reg_stack.top();
+    reg_stack.pop();
+    reg_map.insert({(koopa_raw_binary_t *)&binary, reg});
+
+    // 现在只考虑二元运算的左右值是BINARY还是INTEGER, 在这儿得到左右值在转换为asm形式时对应的字符串
+    bool lhs_is_binary = binary.lhs->kind.tag == KOOPA_RVT_BINARY;
+    bool rhs_is_binary = binary.rhs->kind.tag == KOOPA_RVT_BINARY;
+    bool lhs_is_zero = false, rhs_is_zero = false;
+    std::string lhs, rhs;
+    if (lhs_is_binary)
+        lhs = reg_map[(koopa_raw_binary_t *)&(binary.lhs->kind.data.binary)];
+    else
+    {
+        if (binary.lhs->kind.data.integer.value == 0)
+            lhs_is_zero = true;
+        lhs = std::to_string(binary.lhs->kind.data.integer.value);
+    }
+    if (rhs_is_binary)
+        rhs = reg_map[(koopa_raw_binary_t *)&(binary.rhs->kind.data.binary)];
+    else
+    {
+        if (binary.rhs->kind.data.integer.value == 0)
+            rhs_is_zero = true;
+        rhs = std::to_string(binary.rhs->kind.data.integer.value);
+    }
+
+    switch (binary.op)
+    {
+    case KOOPA_RBO_EQ:
+        if (lhs_is_binary)
+        {
+            if (rhs_is_binary)
+                std::cout << std::setw(6) << "xor" << reg << ", " << lhs << ", " << rhs << std::endl;
+            else
+                std::cout << std::setw(6) << "xori" << reg << ", " << lhs << ", " << rhs << std::endl;
+        }
+        else
+        {
+            if (rhs_is_binary)
+                std::cout << std::setw(6) << "xori" << reg << ", " << rhs << ", " << lhs << std::endl;
+            else
+                std::cout << std::setw(6) << "li" << reg << ", " << (binary.lhs->kind.data.integer.value == binary.rhs->kind.data.integer.value) << std::endl;
+        }
+        if (lhs_is_binary || rhs_is_binary)
+            std::cout << std::setw(6) << "seqz" << reg << ", " << reg << std::endl;
+        break;
+    case KOOPA_RBO_SUB:
+        if(!lhs_is_binary && !rhs_is_binary)
+        {
+            std::cout << std::setw(6) << "li" << reg << ", " << binary.lhs->kind.data.integer.value - binary.rhs->kind.data.integer.value << std::endl;
+            break;
+        }
+        if (!lhs_is_zero && !lhs_is_binary)
+        {
+            lhs = Visit(binary.lhs->kind.data.integer);
+        }
+        if (!rhs_is_binary && !rhs_is_zero)
+        {
+            rhs = Visit(binary.rhs->kind.data.integer);
+            reg_stack.push(rhs);
+        }
+        std::cout << std::setw(6) << "sub" << reg << ", " << lhs << ", " << rhs << std::endl;
+        if (!lhs_is_zero && !lhs_is_binary)
+            reg_stack.push(lhs);
+        break;
+    case KOOPA_RBO_ADD:
+        // if (!lhs_is_binary)
+        // {
+        //     if ()
+        // }
+        break;
+    case KOOPA_RBO_MUL:
+        break;
+    case KOOPA_RBO_DIV:
+        break;
+    case KOOPA_RBO_MOD:
+        break;
+    case KOOPA_RBO_XOR:
+        break;
+    };
 }
 
 // 访问对应类型指令的函数定义略
