cbf6e07155ef150d7c15f476582f96de6a7f0386
kningc <loskningc@outlook.com>
2024-06-05 11:43:44 +0800

    sync

--- test.c
+++ test.c
@@ -1,25 +1,4 @@
-int a = 10;
-
-int inc() {
-  a = a + 1;
-  return a;
-}
-
-void print_a() {
-  putint(a);
-  putch(10);
-}
-
-int main() {
-  int i = 0;
-  while (i < 10) {
-    inc();
-    int a = 1;
-    a = a + 2;
-    putint(a);
-    putch(10);
-    print_a();
-    i = i + 1;
-  }
-  return 0;
+int main(){
+    int a[5] = {1,2,3,4,5};
+    return 4[a];
 }
